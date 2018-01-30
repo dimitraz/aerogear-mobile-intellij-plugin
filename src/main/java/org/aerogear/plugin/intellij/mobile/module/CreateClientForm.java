70f03490b71b15fa26fb9028f3b8eb66ce2eae76
witmicko <mogrodni@redhat.com>
2018-01-30 13:04:33 +0000

    client type const

--- src/main/java/org/aerogear/plugin/intellij/mobile/module/CreateClientForm.java
+++ src/main/java/org/aerogear/plugin/intellij/mobile/module/CreateClientForm.java
@@ -1,21 +1,30 @@
 package org.aerogear.plugin.intellij.mobile.module;
 
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import org.aerogear.plugin.intellij.mobile.constants.Constants;
+import com.intellij.openapi.ui.ComboBox;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class CreateClientForm extends JPanel{
+public class CreateClientForm extends JPanel {
   private JPanel clientPanel;
   private JTextField clientNameTxtField;
-  private JComboBox clientTypescomboBox;
+  private JComboBox clientTypesComboBox;
   private JLabel clientNameLabel;
   private JLabel clientTypeLabel;
 
   public CreateClientForm() {
     super();
+
     add(clientPanel, FlowLayout.LEFT);
   }
 
 
+  private void createUIComponents() {
+    clientTypesComboBox = new ComboBox(Constants.CLIENT_TYPES);
+//    for (String clientType : Constants.CLIENT_TYPES) {
+//      clientTypesComboBox.addItem(clientType);
+//    }
+  }
 }
